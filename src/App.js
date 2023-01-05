f0c5f5f3fdc5e9464a2ffb57d2cc4afe4d858540
Rodrigodcs <rodrigodcsee@gmail.com>
2023-01-04 21:02:45 -0300

    setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react"
+import axios from "axios"
 
 function App() {
+  const [red,setRed] = useState(false)
+  const [green,setGreen] = useState(true)
+
+
+  function redOff(){
+    axios.get("https://parkingtestdeploy.fly.dev/redOff").then(setRed(false))
+  }
+  function redOn(){
+    axios.get("https://parkingtestdeploy.fly.dev/redOn").then(setRed(true))
+  }
+  function greenOff(){
+    axios.get("https://parkingtestdeploy.fly.dev/greenOff").then(setGreen(false))
+  }
+  function greenOn(){
+    axios.get("https://parkingtestdeploy.fly.dev/greenOn").then(setGreen(true))
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-leds">
+      {red?
+        <div className="button-red-on" onClick={() => redOff()}></div>:
+        <div className="button-red-off"onClick={() => redOn()}></div>
+      }
+      {green?
+        <div className="button-green-on" onClick={() => greenOff()}></div>:
+        <div className="button-green-off" onClick={() => greenOn()}></div>
+      }
     </div>
   );
 }
